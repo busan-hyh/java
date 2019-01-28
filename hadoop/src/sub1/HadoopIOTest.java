c0596ddb2ffc12c50b536a6a9d84559a29c646f6
busan-hyh <yeonhwa001@daum.net>
2019-01-28 15:26:35 +0900

    19-01-28 hadoopIOTest

--- hadoop/src/sub1/HadoopIOTest.java
+++ hadoop/src/sub1/HadoopIOTest.java
@@ -3,6 +3,8 @@ package sub1;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 
@@ -12,10 +14,19 @@ public class HadoopIOTest {
 		Configuration conf = new Configuration();
 		
 		FileSystem hdfs = FileSystem.get(conf);
-		Path path = new Path(args[0]);//main실행할때 받는 String[] args의 첫번째. 패스는 여기서 받는다??
+		Path path = new Path(args[0]);//main실행할때 받는 String[] args의 첫번째. 경로 매개변수를 함께 던진다
 		
 		//HDFS 파일쓰기
+		FSDataOutputStream out = hdfs.create(path);
+		out.writeUTF(args[1]);//내용매개변수를 실행할때 함께 던진다.
+		out.close();
 		
 		//HDFS 파일읽기
+		FSDataInputStream in = hdfs.open(path);
+		String txt = in.readUTF();
+		in.close();
+		
+		System.out.println("내용출력 : " + txt);
+		
 	}
 }
